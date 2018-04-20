08f3a1d24bdb3a6febec9beb63a121f0d3ac4b76
Jacob Helfgot <jhelfgot1@gmail.com>
2018-04-20 13:21:06 -0400

    Loaded stylesheet and rendered extension UI into bottom panel of the page

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module) {
    "use strict";
-    console.log("Loaded Extension");
+    console.log("Loaded Css Color Refactoring Extension");
     
     
     var domContent = '<div id="CSS_COLOR_REFACTORING_EXTENSION_DOMCONTENT" class="container bottom-panel"> <div class="chosenColor"> <center> <p>Generate Random Theme</p><p>Chosen Color:</p><span class="dot"></span> <p class="colorHex0">#58355E</p></center> </div><div class="complementaryColors"> <center><p>Four Complementary Colors </p></center> <div class="colors"> <div class="color"> <div class="colorBox1"></div><p class="colorHex1">#E03616</p></div><div class="color"> <div class="colorBox2"></div><p class="colorHex2">#FFF689</p></div><div class="color"> <div class="colorBox3"></div><p class="colorHex3">#CFFFB0</p></div><div class="color"> <div class="colorBox4"></div><p class="colorHex4">#5998C5</p></div></div></div></div>';
@@ -8,7 +8,9 @@ define(function(require, exports, module) {
     var CommandManager = brackets.getModule("command/CommandManager"),
     WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
     Menus = brackets.getModule("command/Menus"),
-    AppInit = brackets.getModule("utils/AppInit");
+    AppInit = brackets.getModule("utils/AppInit"),
+    ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    
     
     var CSS_COLOR_REFACTORING_INIT = "CSS_COLOR_REFACTORING.extensionInit",
         CSS_COlOR_REFACTORING_PANEL = "CSS_COLOR_REFACTORING.panelID";
@@ -39,10 +41,20 @@ define(function(require, exports, module) {
         const viewMenu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
         viewMenu.addMenuDivider();
         viewMenu.addMenuItem(CSS_COLOR_REFACTORING_INIT);
-
+        console.log($(domContent));
 
         panel = WorkspaceManager.createBottomPanel(CSS_COlOR_REFACTORING_PANEL, $(domContent),200);
-        console.log(panel.setVisible(true));
+        
+        
+        const success = function(v) {
+        }
+    
+        const failure = function() {
+            console.log("Failed");
+        }
+        
+        ExtensionUtils.loadStyleSheet(module, "main.css").then(success, failure);
+        panel.setVisible(true);
 
     });
 });
