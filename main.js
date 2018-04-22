1d68bd4cee8ce96b3d558a765d44f0799827e90f
Jacob Helfgot <jhelfgot1@gmail.com>
2018-04-22 19:58:42 -0400

    Made changes to allow html to be read from index.html file rather than set as a string in main.js

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ define(function(require, exports, module) {
     console.log("Loaded Css Color Refactoring Extension");
     
     
-    var domContent = '<div id="CSS_COLOR_REFACTORING_EXTENSION_DOMCONTENT" class="container bottom-panel"> <div class="chosenColor"> <center> <p>Generate Random Theme</p><p>Chosen Color:</p><span class="dot"></span> <p class="colorHex0">#58355E</p></center> </div><div class="complementaryColors"> <center><p>Four Complementary Colors </p></center> <div class="colors"> <div class="color"> <div class="colorBox1"></div><p class="colorHex1">#E03616</p></div><div class="color"> <div class="colorBox2"></div><p class="colorHex2">#FFF689</p></div><div class="color"> <div class="colorBox3"></div><p class="colorHex3">#CFFFB0</p></div><div class="color"> <div class="colorBox4"></div><p class="colorHex4">#5998C5</p></div></div></div></div>';
+    //var domContent = '<div id="CSS_COLOR_REFACTORING_EXTENSION_DOMCONTENT" class="container bottom-panel"> <div class="chosenColor"> <center> <p>Generate Random Theme</p><p>Chosen Color:</p><span class="dot"></span> <p class="colorHex0">#58355E</p></center> </div><div class="complementaryColors"> <center><p>Four Complementary Colors </p></center> <div class="colors"> <div class="color"> <div class="colorBox1"></div><p class="colorHex1">#E03616</p></div><div class="color"> <div class="colorBox2"></div><p class="colorHex2">#FFF689</p></div><div class="color"> <div class="colorBox3"></div><p class="colorHex3">#CFFFB0</p></div><div class="color"> <div class="colorBox4"></div><p class="colorHex4">#5998C5</p></div></div></div></div>';
     
     var CommandManager = brackets.getModule("command/CommandManager"),
     WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
     Menus = brackets.getModule("command/Menus"),
     AppInit = brackets.getModule("utils/AppInit"),
     ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    
+    const modulePath = ExtensionUtils.getModulePath(module);
     
     
     var CSS_COLOR_REFACTORING_INIT = "CSS_COLOR_REFACTORING.extensionInit",
@@ -41,20 +43,47 @@ define(function(require, exports, module) {
         const viewMenu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
         viewMenu.addMenuDivider();
         viewMenu.addMenuItem(CSS_COLOR_REFACTORING_INIT);
-        console.log($(domContent));
 
-        panel = WorkspaceManager.createBottomPanel(CSS_COlOR_REFACTORING_PANEL, $(domContent),200);
-        
-        
-        const success = function(v) {
+        const loadDomContent = function(domContent) {
+            console.log(domContent);
+            console.log(CSS_COlOR_REFACTORING_PANEL);
+            
+            
+            
+            console.log(CSS_COlOR_REFACTORING_PANEL);
+
         }
+        
+        const readHtmlFileError = function(err) {
+            console.log(err);
+            
+        }
+        ExtensionUtils.loadFile(module, "index.html").then(function (fileContents) {
+            console.log(fileContents);
+            panel = WorkspaceManager.createBottomPanel(CSS_COlOR_REFACTORING_PANEL,$(fileContents),200);
+            
+            const success = function(v) {
+                
+                console.log("success");
+                const colorSelector = $("#CSS_COLOR_REFACTORING_EXTENSION_DOMCONTENT.colorPicker");
+                console.log(colorSelector);
+                colorSelector.bind("click", function(evt) {
+                    console.log("Clicked", evt);
+                });
+                
+                const colorBox1 = $(".colorBox1");
+                console.log(colorBox1[0].style);
+                //console.log(colorBox1.style("background-color"));
+                
+            }
     
-        const failure = function() {
-            console.log("Failed");
-        }
+            const failure = function() {
+                console.log("Failed");
+            }
+            
+            ExtensionUtils.loadStyleSheet(module, "main.css").then(success, failure);
+        }, readHtmlFileError);
         
-        ExtensionUtils.loadStyleSheet(module, "main.css").then(success, failure);
-        panel.setVisible(true);
-
+    
     });
 });
