c9ea84c09d2c866033aac7b55e4f318b70cad96c
Jacob Helfgot <jhelfgot1@gmail.com>
2018-04-22 20:52:18 -0400

    Added color palette image

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 define(function(require, exports, module) {
    "use strict";
     console.log("Loaded Css Color Refactoring Extension");
-    
-    
-    //var domContent = '<div id="CSS_COLOR_REFACTORING_EXTENSION_DOMCONTENT" class="container bottom-panel"> <div class="chosenColor"> <center> <p>Generate Random Theme</p><p>Chosen Color:</p><span class="dot"></span> <p class="colorHex0">#58355E</p></center> </div><div class="complementaryColors"> <center><p>Four Complementary Colors </p></center> <div class="colors"> <div class="color"> <div class="colorBox1"></div><p class="colorHex1">#E03616</p></div><div class="color"> <div class="colorBox2"></div><p class="colorHex2">#FFF689</p></div><div class="color"> <div class="colorBox3"></div><p class="colorHex3">#CFFFB0</p></div><div class="color"> <div class="colorBox4"></div><p class="colorHex4">#5998C5</p></div></div></div></div>';
+   
     
     var CommandManager = brackets.getModule("command/CommandManager"),
     WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
     Menus = brackets.getModule("command/Menus"),
     AppInit = brackets.getModule("utils/AppInit"),
-    ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
+    colorPalettePath = require.toUrl("colorpicker.jpg");
+    
     
     const modulePath = ExtensionUtils.getModulePath(module);
     
@@ -24,11 +24,13 @@ define(function(require, exports, module) {
     
     
     function handleMenuToggle() {
+        
         if (panel.isVisible()) {
             panel.hide();
             
             CommandManager.get(CSS_COLOR_REFACTORING_INIT).setChecked(false);
         }
+        
         else {
             panel.show();
             
@@ -45,35 +47,36 @@ define(function(require, exports, module) {
         const viewMenu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
         viewMenu.addMenuDivider();
         viewMenu.addMenuItem(CSS_COLOR_REFACTORING_INIT);
-
-        const loadDomContent = function(domContent) {
-            console.log(domContent);
-            console.log(CSS_COlOR_REFACTORING_PANEL);
+        
+        
+        //function takes a base hexcode and 
+        const getComplementaryColors = function(baseColor) {
             
-            
-            
-            console.log(CSS_COlOR_REFACTORING_PANEL);
-
+            return ["#008000", "#00FFFF", "#008080", "#0000FF"]
         }
-    
         
         const readHtmlFileError = function(err) {
             console.log(err);        
         }
         
         ExtensionUtils.loadFile(module, "index.html").then(function (fileContents) {
+            
             console.log(fileContents);
             panel = WorkspaceManager.createBottomPanel(CSS_COlOR_REFACTORING_PANEL,$(fileContents),200);
             
             const success = function(v) {
                 
                 console.log("success");
-                const colorSelector = $("#CSS_COLOR_REFACTORING_EXTENSION_DOMCONTENT.colorPicker");
-                console.log(colorSelector);
-                colorSelector.bind("click", function(evt) {
-                    console.log("Clicked", evt);
-                });
+                const colorSelectorDot = $("#CSS_COLOR_REFACTORING_EXTENSION_DOMCONTENT #dot");
+                console.log(colorSelectorDot);
                 
+                //Click handler for handling change of selected color.
+                
+                const chosenColorCenter = $("#CSS_COLOR_REFACTORING_EXTENSION_DOMCONTENT #chosenColorCenter");
+                const colorPaletteElement = $("<img id='colorPalette'>");
+                colorPaletteElement.attr("src", colorPalettePath);
+            
+                chosenColorCenter.append(colorPaletteElement);
                 //Sample jquery call.
                 const colorBox1 = $("#colorBox1");
                 colorBox1[0].style.backgroundColor = "blue";
